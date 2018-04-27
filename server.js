691ae43ee8df3d7f0d2f80ec211aabb626d73f2e
Kevin <omallek@gmail.com>
2018-04-27 15:20:00 -0500

    setting up server for example

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+//const models = require('./models');
+const app = require('./app');
+const server = http.createServer(app);
+
+const PORT = 3000;
+
+const init = () => {
+  //sync creates the table if it does not exist. alter true creates the tables and makes any changes to keep the modules in sync
+  server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+}
+
+init();
+
--- server/app.js
+++ server/app.js
@@ -1,36 +1,19 @@
-'use strict';
-const path = require('path');
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(morgan("dev")); //logging middleware
+app.use(express.static(path.join(__dirname, "./client"))); //serving up static files (e.g. css files)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//app.use("/wiki", require("./routes/wiki"));
+//app.use("/users", require("./routes/users"));
+
+app.get('/', function (req, res) {
+  res.send('hello')
+});
+
 module.exports = app;
-
-
-//app.use('/api', require('./routes'));
-
-
-/*
- This middleware will catch any URLs resembling a file extension
- for example: .js, .html, .css
- This allows for proper 404s instead of the wildcard '/*' catching
- URLs that bypass express.static because the given file does not exist.
- */
-app.use(function (req, res, next) {
-
-    if (path.extname(req.path).length > 0) {
-        res.status(404).end();
-    } else {
-        next(null);
-    }
-
-});
-
-app.get('/*', function (req, res) {
-    res.sendFile(app.get('indexHTMLPath'));
-});
-
-// Error catching endware.
-app.use(function (err, req, res, next) {
-    console.error(err, typeof next);
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.');
-});
